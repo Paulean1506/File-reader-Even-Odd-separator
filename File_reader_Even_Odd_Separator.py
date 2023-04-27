090f1639b95b98f07902d1a04424ae0af7526d39
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-04-27 14:05:47 +0800

    header design

---
+++ File_reader_Even_Odd_Separator.py
@@ -0,0 +1,11 @@
+# Paulean Marguerette F. Parrish
+# Bscpe 1-4
+# Problem no. 1
+# Write a Python program that reads a text file named numbers.txt that contains 20 integers. 
+# The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted 
+# from the numbers.txt. The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
+
+# Header design
+import pyfiglet
+
+print(" \033[93mHello! I hope you're having a great day!!! ".center(70, "="))
